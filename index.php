2b959b3732787d9d957b8c249a827428ba3d6cb8
widespread0-0 <danielolugbule@gmail.com>
2025-01-01 14:50:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
 <?php
-require 'query.php';
+require 'conn.php';
+session_start();
 ?>
 
-
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-    <title>Registration and Login</title>
+    <title>Polling System</title>
 </head>
 <body>
     <h2>Register</h2>
-   <?php echo '<form method="POST" action="'.register($conn).'">'; ?>
-        <label for="fullname">Full Name:</label>
-        <input type="text" id="fullname" name="fullname" required><br>
-        <label for="reg">Reg No:</label>
-        <input type="text" id="reg" name="reg" required><br>
+    <form method="POST" action="register.php">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
         <label for="email">Email:</label>
         <input type="email" id="email" name="email" required><br>
-        <label for="serial">Serial:</label>
-        <input type="text" id="serial" name="serial" required><br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br>
         <button type="submit">Register</button>
     </form>
 
     <h2>Login</h2>
-       <?php echo '<form method="POST" action="'login($conn)'">' ; ?>
-        <label for="reg">Reg No:</label>
-        <input type="text" id="reg" name="reg" required><br>
+    <form method="POST" action="login.php">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br>
         <button type="submit">Login</button>
     </form>
 </body>
 </html>
+
+
+    
